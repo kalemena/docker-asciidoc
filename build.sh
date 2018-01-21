a6c9eb659e71612d53418eb518e7903995f31072
kalemena <kalemena@free.fr>
2018-01-21 21:11:07 +0100

    update doc scripts

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ function f
 { 
     echo $1;
     dest="${1/documents/documents-rendered}"
-    asciidoctor -a toc -a data-uri -a icons -D /$dest/ $1/*.adoc
-    asciidoctor-pdf -a icons -D /$dest/ $1/*.adoc
+    asciidoctor -a toc -a data-uri -a icons -D /$dest/ -B /documents $1/*.adoc
+    # asciidoctor-pdf -a icons -D /$dest/ -B /documents $1/*.adoc
+    # asciidoctor-confluence --host http://confluence:8090 --update --spaceKey ${CONFLUENCE_SPACEKEY} --title ${CONFLUENCE_TITLE} --username ${CONFLUENCE_USERNAME} --password ${CONFLUENCE_PASSWORD} /documents/demo1.adoc
 
     for i in $1/*; do 
         if [ -d $i ]; then 
-            # echo $i; 
-            f $i; 
+            if [[ ! "$i" =~ "images" ]] && [[ ! "$i" =~ "includes" ]]; then               
+                f $i;
+            fi; 
         fi; 
     done 
 }
