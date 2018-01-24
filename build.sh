02a022663085cd59b70785812b6b4a2476a7ee78
kalemena <kalemena@free.fr>
2018-01-24 21:32:28 +0100

    enable pdf

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ function f
     echo $1;
     dest="${1/documents/documents-rendered}"
     asciidoctor -a toc -a data-uri -a icons -D /$dest/ -B /documents $1/*.adoc
-    # asciidoctor-pdf -a icons -D /$dest/ -B /documents $1/*.adoc
+    asciidoctor-pdf -a icons -D /$dest/ -B /documents $1/*.adoc
     # asciidoctor-confluence --host http://confluence:8090 --update --spaceKey ${CONFLUENCE_SPACEKEY} --title ${CONFLUENCE_TITLE} --username ${CONFLUENCE_USERNAME} --password ${CONFLUENCE_PASSWORD} /documents/demo1.adoc
 
     for i in $1/*; do 
