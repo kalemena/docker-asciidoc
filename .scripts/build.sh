fb6965a42587228969c08e79e509bb9af8825c01
kalemena <kalemena@free.fr>
2018-03-06 14:36:03 +0100

    Add maven build

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -8,6 +8,7 @@ function f
     if [ -f "$1/readme.adoc" ]; then
         asciidoctor -a toc -a toc=left -a docinfo=shared -a data-uri -a icons -r asciidoctor-diagram -D /$dest/ -B /documents $1/readme.adoc
         asciidoctor-pdf -a icons -r asciidoctor-diagram -D /$dest/ -B /documents $1/readme.adoc
+        # does not work well !!!
         # asciidoctor-confluence --host http://confluence:8090 --update --spaceKey ${CONFLUENCE_SPACEKEY} --title ${CONFLUENCE_TITLE} --username ${CONFLUENCE_USERNAME} --password ${CONFLUENCE_PASSWORD} /documents/demo1.adoc
     fi
 
@@ -23,5 +24,8 @@ function f
 # copy all
 cp -r /documents/* /documents-rendered
 
-# then process
+# then process HTML and PDF
 f /documents
+
+# then process publish to confluence
+mvn -e -X org.sahli.asciidoc.confluence.publisher:asciidoc-confluence-publisher-maven-plugin:publish
